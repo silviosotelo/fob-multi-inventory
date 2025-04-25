1551b4fe7367c0fa220af2432c074b67a54e079e
Silvio Sotelo <silvio.sotelo@hotmail.com>
2025-04-25 11:39:35 -0300

    correcciones y migraciones

--- src/Tables/InventoryTable.php
+++ src/Tables/InventoryTable.php
@@ -3,13 +3,21 @@
 namespace FriendsOfBotble\MultiInventory\Tables;
 
 use Botble\Base\Enums\BaseStatusEnum;
-use Botble\Base\Facades\BaseHelper;
-use Botble\Base\Facades\Html;
 use Botble\Table\Abstracts\TableAbstract;
-use Botble\Table\DataTables;
+use Botble\Table\Actions\DeleteAction;
+use Botble\Table\Actions\EditAction;
+use Botble\Table\BulkActions\DeleteBulkAction;
+use Botble\Table\BulkChanges\CreatedAtBulkChange;
+use Botble\Table\BulkChanges\NameBulkChange;
+use Botble\Table\BulkChanges\StatusBulkChange;
+use Botble\Table\Columns\CreatedAtColumn;
+use Botble\Table\Columns\IdColumn;
+use Botble\Table\Columns\LinkableColumn;
+use Botble\Table\Columns\StatusColumn;
+use Botble\Table\Columns\YesNoColumn;
+use Botble\Table\HeaderActions\CreateHeaderAction;
 use FriendsOfBotble\MultiInventory\Models\Inventory;
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Http\JsonResponse;
 
 class InventoryTable extends TableAbstract
 {
@@ -17,133 +25,40 @@ class InventoryTable extends TableAbstract
     {
         $this
             ->model(Inventory::class)
+            ->addHeaderAction(
+                CreateHeaderAction::make()->route('multi-inventory.inventories.create')
+            )
             ->addActions([
-                'edit' => [
-                    'permission' => 'multi-inventory.inventories.edit',
-                    'route' => 'multi-inventory.inventories.edit',
-                    'icon' => 'fa fa-edit',
-                    'label' => trans('core/base::tables.edit'),
-                ],
-                'delete' => [
-                    'permission' => 'multi-inventory.inventories.destroy',
-                    'route' => 'multi-inventory.inventories.destroy',
-                    'icon' => 'fa fa-trash',
-                    'label' => trans('core/base::tables.delete'),
-                    'class' => 'btn-danger',
-                ],
-            ]);
-    }
-
-    public function ajax(): JsonResponse
-    {
-        $data = $this->table
-            ->eloquent($this->query())
-            ->editColumn('name', function (Inventory $item) {
-                return Html::link(route('multi-inventory.inventories.edit', $item->id), BaseHelper::clean($item->name));
-            })
-            ->editColumn('is_default', function (Inventory $item) {
-                return $item->is_default ? trans('core/base::base.yes') : trans('core/base::base.no');
-            })
-            ->editColumn('is_frontend', function (Inventory $item) {
-                return $item->is_frontend ? trans('core/base::base.yes') : trans('core/base::base.no');
-            })
-            ->editColumn('is_backend', function (Inventory $item) {
-                return $item->is_backend ? trans('core/base::base.yes') : trans('core/base::base.no');
-            })
-            ->editColumn('checkbox', function (Inventory $item) {
-                return $this->getCheckbox($item->id);
-            })
-            ->editColumn('created_at', function (Inventory $item) {
-                return BaseHelper::formatDate($item->created_at);
-            })
-            ->editColumn('status', function (Inventory $item) {
-                return $item->status->toHtml();
-            });
-
-        return $this->toJson($data);
-    }
-
-    public function query(): Builder
-    {
-        $query = $this->getModel()->query()->select([
-            'id',
-            'name',
-            'is_default',
-            'is_frontend',
-            'is_backend',
-            'created_at',
-            'status',
-        ]);
-
-        return $this->applyScopes($query);
-    }
-
-    public function columns(): array
-    {
-        return [
-            'id' => [
-                'title' => trans('core/base::tables.id'),
-                'width' => '20px',
-            ],
-            'name' => [
-                'title' => trans('core/base::tables.name'),
-                'class' => 'text-start',
-            ],
-            'is_default' => [
-                'title' => trans('plugins/multi-inventory::multi-inventory.is_default'),
-                'width' => '100px',
-            ],
-            'is_frontend' => [
-                'title' => trans('plugins/multi-inventory::multi-inventory.is_frontend'),
-                'width' => '100px',
-            ],
-            'is_backend' => [
-                'title' => trans('plugins/multi-inventory::multi-inventory.is_backend'),
-                'width' => '100px',
-            ],
-            'created_at' => [
-                'title' => trans('core/base::tables.created_at'),
-                'width' => '100px',
-            ],
-            'status' => [
-                'title' => trans('core/base::tables.status'),
-                'width' => '100px',
-            ],
-        ];
-    }
-
-    public function buttons(): array
-    {
-        return $this->addCreateButton(route('multi-inventory.inventories.create'), 'multi-inventory.inventories.create');
-    }
-
-    public function bulkActions(): array
-    {
-        return $this->addDeleteAction(
-            route('multi-inventory.inventories.deletes'),
-            'multi-inventory.inventories.destroy',
-            parent::bulkActions()
-        );
-    }
-
-    public function getBulkChanges(): array
-    {
-        return [
-            'name' => [
-                'title' => trans('core/base::tables.name'),
-                'type' => 'text',
-                'validate' => 'required|max:120',
-            ],
-            'status' => [
-                'title' => trans('core/base::tables.status'),
-                'type' => 'select',
-                'choices' => BaseStatusEnum::labels(),
-                'validate' => 'required|in:' . implode(',', BaseStatusEnum::values()),
-            ],
-            'created_at' => [
-                'title' => trans('core/base::tables.created_at'),
-                'type' => 'datePicker',
-            ],
-        ];
+                EditAction::make()->route('multi-inventory.inventories.edit'),
+                DeleteAction::make()->route('multi-inventory.inventories.destroy'),
+            ])
+            ->addColumns([
+                IdColumn::make(),
+                LinkableColumn::make('name')
+                    ->route('multi-inventory.inventories.edit'),
+                YesNoColumn::make('is_default')
+                    ->title(trans('plugins/multi-inventory::multi-inventory.is_default')),
+                YesNoColumn::make('is_frontend')
+                    ->title(trans('plugins/multi-inventory::multi-inventory.is_frontend')),
+                YesNoColumn::make('is_backend')
+                    ->title(trans('plugins/multi-inventory::multi-inventory.is_backend')),
+                CreatedAtColumn::make(),
+                StatusColumn::make(),
+            ])
+            ->addBulkChanges([
+                NameBulkChange::make(),
+                StatusBulkChange::make(),
+                CreatedAtBulkChange::make(),
+            ])
+            ->addBulkAction(DeleteBulkAction::make())
+            ->queryUsing(fn (Builder $query) => $query->select([
+                'id',
+                'name',
+                'is_default',
+                'is_frontend',
+                'is_backend',
+                'created_at',
+                'status',
+            ]));
     }
 }
